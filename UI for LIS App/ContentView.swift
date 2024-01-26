c5e259fa3b01827469417d5ab5b95264cb909f4f
CalvinBredesen <ctbredesen24@blakeschool.org>
2024-01-26 09:05:49 -0600

    Initial Commit

---
+++ UI for LIS App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UI for LIS App
+//
+//  Created by Calvin Bredesen24 on 1/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
