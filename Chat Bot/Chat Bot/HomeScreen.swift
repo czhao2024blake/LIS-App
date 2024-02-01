5377f3b707327fea771ac34b3d49dd3ebbcccab3
cswambold24 <cswambold24@blakeschool.org>
2024-01-31 18:06:46 -0600

    Added what czhao did, and connected it with other home screen.

---
+++ Chat Bot/Chat Bot/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  Chat Bot
+//
+//  Created by Carver Wambold24 on 1/31/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
