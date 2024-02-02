6f010726fe0dcad7caa2331025e8f33601a853fc
cswambold24 <122554788+cswambold24@users.noreply.github.com>
2024-02-02 09:17:03 -0600

    Update HomeViewModel.swift

--- Chat Bot/Chat Bot/HomeViewModel.swift
+++ Chat Bot/Chat Bot/HomeViewModel.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct HomeViewModel {
+    func getLanguageIndex(_ language: String) -> Int? {
+        let languages = ["English", "Español", "Français"]
+        for i in 0...languages.count-1 {
+            if languages[i] == language {
+                return i
+            }
+        }
+        print("Language not found.")
+        return nil
+    }
+}
